da65f140fbe181850a4e1835c9f8a2ae352098dd
KennyDiMel <diestrafernando2024@gmail.com>
2024-08-16 05:57:24 +0000

    He agreagado el componente Titulo en la web principal

--- WebTest/src/componentes/Titulo.jsx
+++ WebTest/src/componentes/Titulo.jsx
@@ -4,7 +4,7 @@ function Titulo (){
     return (
         <>
         <h1>Conoce el Perú primero</h1>
-        <h2>En el Perú existe hermosos lugares para conocer y disfrutar</h2>
+        <h2>En el Perú existen hermosos lugares para conocer y disfrutar</h2>
         </>
     );
 }
