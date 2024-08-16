840c9cd0c3de40273f65435b16f788434222a860
KennyDiMel <diestrafernando2024@gmail.com>
2024-08-16 05:11:14 +0000

    Hemos trabajado con dos componentes

--- HelloKenny/src/App.jsx
+++ HelloKenny/src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react"
 import HolaKenny from "./componentes/HolaKenny";
+import Mensaje from "./componentes/Mensaje";
 
 function App(){
 
   return (
     <>
       <HolaKenny />
+      <Mensaje />
     </>
   );
 }
