0d60a7d05aae66858e87c40abe91fb3c51a90e75
KennyDiMel <diestrafernando2024@gmail.com>
2024-08-17 18:31:12 +0000

    He agregado el componente menu en la web principal

---
+++ Traveling/src/componentes/Menu.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from 'react';
+import './Menu.css';
+
+function Menu () {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>Quiénes somos</li>
+        <li>Destinos</li>
+        <li>Servicios</li>
+        <li>Ofertas</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
